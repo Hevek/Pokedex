f7446ba172cbae0ff6a0435eb5812340513edd9d
Hevek <rkkemps@gmail.com>
2022-05-04 09:29:13 -0300

    first commit

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,267 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Poke.Models;
+
+namespace Poke.Controllers
+{
+    public class PokemonController : Controller
+    {
+        private readonly Poke.Data.AppContext _appContext;
+        public PokemonController(Poke.Data.AppContext appContext)
+        {
+            _appContext = appContext;
+        }
+
+        public async Task<IActionResult> Home1()
+        {
+            var trainers = await _appContext.PokesM.ToListAsync();
+            return View(trainers);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create (TrainerM trainer, IList<IFormFile> Img)
+        {
+            //Verificar tamanho da imagem
+            IFormFile uploadedImage = Img.FirstOrDefault();
+            MemoryStream ms = new MemoryStream();
+            if(Img.Count > 0)
+            {
+                uploadedImage.OpenReadStream().CopyTo(ms);
+                trainer.Picture = ms.ToArray();
+            }
+            _appContext.TrainersM.Add(trainer);
+           await _appContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult CreateP()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateP (PokeM pokemon, IList<IFormFile> ImgP)
+        {
+            IFormFile uploadedImage = ImgP.FirstOrDefault();
+            MemoryStream ms = new MemoryStream();
+            if (ImgP.Count > 0)
+            {
+                uploadedImage.OpenReadStream().CopyTo(ms);
+                pokemon.PictureP = ms.ToArray();
+            }
+            _appContext.PokesM.Add(pokemon);
+           await _appContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DetailsTrainer (Guid? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var trainers = await _appContext.TrainersM.FindAsync(id);
+
+            if(trainers == null)
+            {
+                return BadRequest();
+            }
+
+            return View(trainers);
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DetailsPokemon (Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var poke = await _appContext.PokesM.FindAsync(id);
+
+            if (poke == null)
+            {
+                return BadRequest();
+            }
+            return View(poke);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditTrainers (Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trainers = await _appContext.TrainersM.FindAsync(id);
+
+            if (trainers == null)
+            {
+                return BadRequest();
+            }
+
+            return View(trainers);
+
+        }
+        
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditTrainers(Guid? id, TrainerM trainer, IList<IFormFile> Img)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var Olddata = _appContext.TrainersM.AsNoTracking().FirstOrDefault(t => t.IdT == id);
+
+            IFormFile uploadedImage = Img.FirstOrDefault();
+            MemoryStream ms = new MemoryStream();
+            if (Img.Count > 0)
+            {
+                uploadedImage.OpenReadStream().CopyTo(ms);
+                trainer.Picture = ms.ToArray();
+            }
+            else
+            {
+                trainer.Picture = Olddata.Picture;
+            }
+            if (ModelState.IsValid)
+            {
+                _appContext.Update(trainer);
+                await _appContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(trainer);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditPokemon(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var poke = await _appContext.PokesM.FindAsync(id);
+
+            if (poke == null)
+            {
+                return BadRequest();
+            }
+
+            return View(poke);
+
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditPokemon(Guid? id, PokeM pokemon, IList<IFormFile> ImgP)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var OlddataP = _appContext.PokesM.AsNoTracking().FirstOrDefault(p => p.IdP == id);
+
+            IFormFile uploadedImage = ImgP.FirstOrDefault();
+            MemoryStream ms = new MemoryStream();
+            if (ImgP.Count > 0)
+            {
+                uploadedImage.OpenReadStream().CopyTo(ms);
+                pokemon.PictureP = ms.ToArray();
+            }
+            else
+            {
+                pokemon.PictureP = OlddataP.PictureP;
+            }
+            if (ModelState.IsValid)
+            {
+                _appContext.Update(pokemon);
+                await _appContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pokemon);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteTrainer (Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trainerD = await _appContext.TrainersM.FindAsync(id);
+            if (trainerD == null)
+            {
+                return BadRequest();
+            }
+            return View(trainerD);
+
+        }
+
+        [HttpPost, ActionName("Delete Trainer")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmedT(Guid? id)
+        {
+            var trainerDel = await _appContext.TrainersM.FindAsync(id);
+            _appContext.TrainersM.Remove(trainerDel);
+            await _appContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeletePokemon(Guid? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var pokemonD = await _appContext.PokesM.FindAsync(id);
+            if(pokemonD == null)
+            {
+                return BadRequest();
+            }
+            return View(pokemonD);
+        }
+        [HttpPost,ActionName("DeletePokemon")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmedP(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var pokemonDel = await _appContext.PokesM.FindAsync(id);
+            if (pokemonDel == null)
+            {
+                return BadRequest();
+            }
+            _appContext.PokesM.Remove(pokemonDel);
+            await _appContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        //var pokemonDel = await _appContext.PokesM.FindAsync(id);
+        //_appContext.PokesM.Remove(pokemonDel);
+        //await _appContext.SaveChangesAsync();
+        //return RedirectToAction("Index");
+    }
+}
---
+++ README.txt
@@ -0,0 +1,34 @@
+.LOG
+
+18:46 05/04/2022 
+ >>
+ Abri uma segunda função no projeto para inserir os dados de cada
+ pokémon, e também de cada treinador. Ainda não corrigi os erros 
+ antigos.
+ >>
+~~~~
+ Trocar trainer em tudo por pokémon! o usuário irá cadastrar
+ pokémons mas será que poderiamos cadastrar treinadores também?
+ Pense nisso. 
+
+ Fazer upload do projeto no GitHub depois. 
+ Rever as paginas salvas de pokemon para a UI do projeto.
+ Conseguir as fontes para a interface ou criá-las.
+ 
+21:18 06/04/2022
+>>
+ Consegui fazer muita coisa, mas não consegui trocar para a fonte
+ do pokemon.
+>>
+17:27 08/04/2022
+Colocar fonte ddo pokemon na tela de carregamento, tirar a barra de
+ pesquisa
+15:31 10/04/2022
+feito.
+15:31 10/04/2022
+
+01:10 28/04/2022
+terminar de criar as views
+depois arrumar o problema de redirecionar por id
+e arrumar os cards
+14:07 28/04/2022
